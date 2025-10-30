0583939c08cf6e22c55cb60372ea3867430f77fd
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2025-10-30 00:43:44 -0700

    Add side bar and navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Navbar from "@/components/ui/Navbar";
+import AppSidebar from "@/components/AppSidebar";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +28,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AppSidebar/>
+        <main className="">
+          <Navbar/>
+          <div className="p-4">{children}</div>
+       
+        </main>
+
       </body>
     </html>
   );
