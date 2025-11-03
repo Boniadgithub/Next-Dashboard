fcb4824c43bee47f18e91e29ab6546aec5f3229a
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2025-11-03 05:12:53 -0800

    add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import { Button } from "@/components/ui/button"
-import {Plus} from "lucide-react"
+
 const Homepage =()=>{
   return(
-  <div className="">
-       
+  <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2">Test</div>
+       <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+       <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+       <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2">Test</div>
+       <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
   </div>
   )
 }
